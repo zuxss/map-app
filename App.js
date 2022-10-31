1ed1042db1ca546da7958ec921f011e10ba7661a
Franco Pacheco <pachecozux@gmail.com>
2022-10-31 11:03:26 -0300

    commit inicial,mapa con geolocalización

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import * as Location from "expo-location";
+
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions, Platform } from "react-native";
 
 export default function App() {
+  const [origin, setOrigin] = React.useState({
+    latitude: 33.4412412,
+    longitude: 33.123123,
+  });
+  React.useEffect(() => {
+    getLocationPermission();
+  }, []);
+
+  async function getLocationPermission() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Denegado");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+    setOrigin(current);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      >
+        <Marker draggable coordinate={origin} />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +46,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
