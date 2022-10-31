d6b6785aaef663cd2fc3512b142b3b757596943b
Franco Pacheco <pachecozux@gmail.com>
2022-10-31 16:25:21 -0300

    añadida lista de milas y estilos generales

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
-
 import MapView, { Marker } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Platform,
+  ScrollView,
+  Button,
+  StatusBar,
+  Image,
+} from "react-native";
+
+import locations from "./importantLocations";
 
 export default function App() {
-  const [origin, setOrigin] = React.useState({
+  const [origin, setOrigin] = useState({
     latitude: 33.4412412,
     longitude: 33.123123,
   });
-  React.useEffect(() => {
+
+  useEffect(() => {
     getLocationPermission();
   }, []);
 
+  function getLocationSangu(lugar) {
+    const coordenada = {
+      latitude: lugar.latitude,
+      longitude: lugar.longitude,
+    };
+    setOrigin(coordenada);
+  }
   async function getLocationPermission() {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
@@ -28,17 +47,61 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
+      <StatusBar></StatusBar>
+      <View>
+        <Image
+          source={require("./assets/Map-Milas2.png")}
+          style={{ width: Dimensions.get("window").width, height: 100 }}
+        ></Image>
+      </View>
+
       <MapView
         style={styles.map}
         initialRegion={{
           latitude: origin.latitude,
           longitude: origin.longitude,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.04,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.09,
+        }}
+        region={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
         }}
       >
-        <Marker draggable coordinate={origin} />
+        <Marker coordinate={origin} />
+        {locations.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+            title={marker.name}
+            description={marker.description}
+          ></Marker>
+        ))}
       </MapView>
+
+      <ScrollView>
+        <View key="home" style={styles.listItem}>
+          <Text style={{ color: "white" }}>Ubicación Actual</Text>
+          <Button title="Ir" onPress={getLocationPermission}></Button>
+        </View>
+        {locations.map((marker) => (
+          <View key={marker.id} style={styles.listItem}>
+            <Text style={{ color: "white" }}>{marker.name}</Text>
+            <Button
+              key={marker.id}
+              title={"Ir"}
+              onPress={() => {
+                getLocationSangu(marker);
+              }}
+            ></Button>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -46,12 +109,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "black",
   },
   map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    alignSelf: "center",
+    width: Dimensions.get("window").width * 0.9,
+    height: Dimensions.get("window").height * 0.5,
+    marginBottom: 10,
+  },
+
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "white",
+    width: "100%",
+    backgroundColor: "black",
+    margin: 5,
   },
 });
---
+++ importantLocations.js
@@ -0,0 +1,45 @@
+const locations = [
+  {
+    id: "1",
+    latitude: -26.838192683139862,
+    longitude: -65.23115441166934,
+    name: "Kuky",
+    description: "Av. Gral. Roca 1980",
+  },
+  {
+    id: "2",
+    latitude: -26.808464859549197,
+    longitude: -65.249361290975,
+    name: "El Turco",
+    description: "Av. Manuel Belgrano, Diagonal 1",
+  },
+  {
+    id: "3",
+    latitude: -26.81768070935617,
+    longitude: -65.26668601595452,
+    name: "El 10",
+    description: " Av. Aconquija 153, T4107 Yerba Buena",
+  },
+  {
+    id: "4",
+    latitude: -26.815339001968677,
+    longitude: -65.21385266266414,
+    name: "Los Eléctricos",
+    description: "Batalla de Suipacha 950",
+  },
+  {
+    id: "5",
+    latitude: -26.836290612777137,
+    longitude: -65.205451995572,
+    name: "Don Pepe",
+    description: "Gral. Paz 463",
+  },
+  {
+    id: "6",
+    latitude: -26.795889156972716,
+    longitude: -65.21074188675017,
+    name: "Fiky",
+    description: "Av. República del Líbano 2320",
+  },
+];
+export default locations;
